784a6019ce3e542b5ad3a27ac23e7d570040354a
NataChasovskikh <flosalnatali@gmail.com>
2019-05-21 19:28:28 +0300

    node day1 and day2

---
+++ Day_1/01_Introduce to NodeJS/111/index.js
@@ -0,0 +1,20 @@
+// let colors = require('colors');
+
+// let text = 'Hello student!';
+// console.log(text.rainbow);
+// console.log(text.zebra.italic);
+// console.log(text.america);
+// console.log(text.trap.dim);
+// console.log(text.dim);
+// console.log(text.bold.magenta);
+
+
+console.log(process.execPath);
+console.log(process.version);
+console.log(process.platform);
+console.log(process.arch);
+console.log(process.title);
+console.log(process.pid);
+
+
+
